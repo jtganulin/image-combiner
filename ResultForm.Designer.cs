a3891e96654588113ee3a66e72a7fefb11f96a0d
Jeffrey Ganulin <45239282+jtganulin@users.noreply.github.com>
2022-07-29 17:21:52 -0400

    Cleaned up UI. Fix toggling of image clear button.

--- ResultForm.Designer.cs
+++ ResultForm.Designer.cs
@@ -209,6 +209,7 @@
 			this.btnSave.Name = "btnSave";
 			this.btnSave.Size = new System.Drawing.Size(181, 73);
 			this.btnSave.TabIndex = 1;
+			this.btnSave.TabStop = false;
 			this.btnSave.Text = "Save Image...";
 			this.btnSave.UseVisualStyleBackColor = true;
 			this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
